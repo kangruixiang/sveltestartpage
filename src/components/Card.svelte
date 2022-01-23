10eb95e3a609c206c2d7e48ff02c05335a4a66a4
Kang <thisispiggy@gmail.com>
2022-01-23 00:29:59 -0500

    added animations

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -9,7 +9,7 @@
 {#each site as site}
 	<div class="flex flex-row">
 		<div
-			class="text-lg py-2 px-2 w-full mx-2 cursor-pointer text-stone-200 hover:bg-stone-900 hover:rounded-lg"
+			class="text-lg py-2 px-2 w-full mx-2 cursor-pointer text-stone-200 hover:bg-stone-900 hover:rounded-lg transition duration-150"
 			onclick="location.href='{site.url}'
 			">
 			{site.name}
