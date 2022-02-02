50393d4f4c2f04f9feb9650659eaf34b1ec80eb8
Kang <thisispiggy@gmail.com>
2022-02-02 10:05:05 -0500

    darker highlight

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -11,7 +11,7 @@
 {#each site as site}
 	<div class="flex flex-row">
 		<a
-			class="w-full px-2 py-2 mx-2 text-lg cursor-pointer text-stone-200 hover:rounded-lg hover:text-stone-100 hover:bg-stone-800"
+			class="w-full px-2 py-2 mx-2 text-lg cursor-pointer text-stone-200 hover:rounded-lg hover:text-stone-100 hover:bg-stone-900"
 			href='{site.url}'
 			>
 			{site.name}
