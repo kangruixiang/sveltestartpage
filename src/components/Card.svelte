d7ab2316cc2114be91dc6eb44a570fa83c167996
Kang <thisispiggy@gmail.com>
2022-02-01 22:34:27 -0500

    back to default

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,7 +1,6 @@
 <script>
 	export let site;
 	export let color;
-	export let randomColor
 	console.log('test', color)
 </script>
 
@@ -12,9 +11,8 @@
 {#each site as site}
 	<div class="flex flex-row">
 		<a
-			class="w-full px-2 py-2 mx-2 text-lg cursor-pointer test text-stone-800 dark:text-stone-300 hover:rounded-lg hover:text-stone-100 hover:bg-blend-darken"
+			class="w-full px-2 py-2 mx-2 text-lg cursor-pointer text-stone-200 hover:rounded-lg hover:text-stone-100 hover:bg-stone-800"
 			href='{site.url}'
-			style="background-color: {randomColor}"
 			>
 			{site.name}
 		</a>
